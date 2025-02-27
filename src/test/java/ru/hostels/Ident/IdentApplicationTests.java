44f734127ffdddb523b41713c3e3637d8739ef52
Haikevich <legobuilder0819@gmail.com>
2025-02-27 10:40:30 +0300

    Initial commit
    # Conflicts:
    #	.gitignore

---
+++ src/test/java/ru/hostels/Ident/IdentApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.hostels.Ident;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
