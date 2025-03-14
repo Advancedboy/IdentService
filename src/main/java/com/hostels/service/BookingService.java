9ff16ba5dbd4e700266f781b6bdaa8aa543d4a3c
Haikevich <legobuilder0819@gmail.com>
2025-03-14 08:24:48 +0300

    exception files

--- src/main/java/com/hostels/service/BookingService.java
+++ src/main/java/com/hostels/service/BookingService.java
@@ -1,6 +1,8 @@
 package com.hostels.service;
 
 import com.hostels.enums.Role;
+import com.hostels.exception.NotFoundException;
+import com.hostels.exception.UnauthorizedActionException;
 import com.hostels.model.Booking;
 import com.hostels.model.Room;
 import com.hostels.model.User;
@@ -28,14 +30,14 @@ public class BookingService {
                                  LocalDate checkIn,
                                  LocalDate checkOut) {
         User client = userRepository.findById(clientId)
-                .orElseThrow(() -> new RuntimeException("Client not found"));
+                .orElseThrow(() -> new NotFoundException("Client not found"));
 
         if (client.getRole() != Role.CLIENT) {
-            throw new RuntimeException("User is not a client");
+            throw new UnauthorizedActionException("User is not a client");
         }
 
         Room room = roomRepository.findById(roomId)
-                .orElseThrow(() -> new RuntimeException("Room not found"));
+                .orElseThrow(() -> new NotFoundException("Room not found"));
 
         Booking booking = new Booking();
         booking.setUser(client);
