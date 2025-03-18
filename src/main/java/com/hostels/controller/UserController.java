445129cf29d26890ea353a53f93dafc19fb784a2
Haikevich <legobuilder0819@gmail.com>
2025-03-18 13:48:24 +0300

    Fixing status code

--- src/main/java/com/hostels/controller/UserController.java
+++ src/main/java/com/hostels/controller/UserController.java
@@ -4,6 +4,8 @@ import com.hostels.model.User;
 import com.hostels.service.UserService;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping(path = "/api/users")
@@ -22,19 +25,22 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping(path = "/data/all")
+    @GetMapping(path = "/all")
     public List<User> findAll() {
         return userService.findAll();
     }
 
-    @GetMapping("/data/email/{email}")
+    @GetMapping("/email/{email}")
     public Optional<User> getUserByEmail(@PathVariable String email) {
         return userService.getUserByEmail(email);
     }
 
-    @GetMapping(path = "/data/{id}")
-    public Optional<User> findById(@PathVariable Long id) {
-        return userService.findById(id);
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        return userService.findById(id)
+                .map(ResponseEntity::ok)
+                .orElseThrow(
+                        () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found"));
     }
 
     @PostMapping(path = "/create")
